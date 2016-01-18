7bee294a1affc5eed7d8972baecca398402dfb34
Vaikesh K P <vaikesh4leo@gmail.com>
2016-01-18 13:03:41 +0530

    Tab Sample
    Tab in Xamarin.android

---
+++ TabHostWalkthrough/TabHostWalkthrough/MyScheduleActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace com.xamarin.example.tabhostwalkthrough
+{
+	[Activity]
+	public class MyScheduleActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			TextView textview = new TextView (this);
+			textview.Text = "This is the My Schedule tab";
+			SetContentView (textview);
+		}
+	}
+}
+
---
+++ TabHostWalkthrough/TabHostWalkthrough/SpeakersActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace com.xamarin.example.tabhostwalkthrough
+{
+	[Activity]
+	public class SpeakersActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			TextView textview = new TextView (this);
+			textview.Text = "This is the Speaker tab";
+			SetContentView (textview);
+		}
+	}
+}
+
